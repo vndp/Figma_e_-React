aeb6293558df6c12a3a7524c40eb67f94bd438ef
Vit <vitoriaperez6@gmail.com>
2022-10-15 08:57:17 -0300

    react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/global.css'
+
+
+export function App() {
+  return (
+<>
+  <h1 className="font-bold text-5xl text-emerald-800 "> Hello Word!</h1>
+    <button className="bg-green-600 font-medium px-5 py-2 rounded text-white hover:bg-green-900">
+      Enviar 
+
+    </button>
+
+</>
+
+  )
+}
